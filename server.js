f6ca0124a451b6e0c86f5b334a18d2d38c6d3f03
Simone Siepi <ssimone.siepi@itis.pr.it>
2024-03-13 21:02:32 +0100

    salvataggio dei dati

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const fs = require("fs").promises; 
+const fs = require("fs").promises;
 const bodyParser = require("body-parser");
 const path = require("path");
 const port = 3000;
@@ -11,75 +11,73 @@ const rottaDati = path.join(__dirname, "public", "dati");
 app.set("view engine", "pug");
 app.set("views", "./view");
 
- 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(rottaAsset));
 app.use("/dati", express.static(rottaDati));
 
 async function read() {
-    try {
-      const datiJson = await fs.readFile("datiMetereologici.json", 'utf-8');
-      return JSON.parse(datiJson);
-    } catch (error) {
-      if (error.code === 'ENOENT') {
-        await fs.writeFile(rottaDati,"datiMetereologici.json", JSON.stringify({}, null, 2), 'utf-8');
-        console.log("File datiMetereologici.json creato.");
-        return {}; // Ritorna un oggetto vuoto
-      } else {
-        console.error("Errore durante la lettura del file:", error);
-        return [];
-      }
+  try {
+    const datiJson = await fs.readFile(path.join(rottaDati, "datiMetereologici.json"),"utf-8");
+    return JSON.parse(datiJson);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      await fs.writeFile(path.join(rottaDati, "datiMetereologici.json"),JSON.stringify([], null, 2),"utf-8");
+      return [];
+    } else {
+      console.error("Errore durante la lettura del file:", error);
+      return [];
     }
   }
-  
-  async function write(datiJson) {
-    try {
-      await fs.writeFile("datiMetereologici.json", JSON.stringify(datiJson, null, 2), 'utf-8');
-    } catch (error) {
-      console.error("Errore durante la scrittura del file:", error);
-    }
+}
+
+async function write(datiJson) {
+  try {
+    await fs.writeFile(path.join(rottaDati, "datiMetereologici.json"), JSON.stringify(datiJson, null, 2), "utf-8");
+  } catch (error) {
+    console.error("Errore durante la scrittura del file:", error);
   }
-  
+}
+
 async function save() {
-    await write(datiMeteo);
+  await write(datiMeteo);
 }
 
 let datiMeteo = read();
 
-app.get("/", (req,res)=>{
-    res.render("index");
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get("/formMeteo",(req,res)=>{
+app.get("/formMeteo", (req, res) => {
   res.render("form");
 });
 
-app.post("/inserisciDati", async (req, res) => {
-  let tempMax = -Infinity;
-  let tempMin = Infinity;
-  let tempAlte = [];
-  let tempBasse = [];
-  datiMeteo.forEach(stazione => {
-    stazione.dati.forEach(dato => {
-      if (dato.temperaturaMassima > tempMax) tempMax = dato.temperaturaMassima;
-      if (dato.temperaturaMinima < tempMin) tempMin = dato.temperaturaMinima;
-      tempAlte.push(dato.temperaturaMassima);
-      tempBasse.push(dato.temperaturaMinima);
+app.post("/inserisciDati", (req, res) => {
+  const { stazione, tempMax, tempMin } = req.body;
+
+  if (!Array.isArray(datiMeteo)) {
+    datiMeteo = [];
+  }
+  const stazioneDati = datiMeteo.find((s) => s.nome === stazione);
+  if (stazioneDati) {
+    stazioneDati.dati.push({
+      temperaturaMassima: parseFloat(tempMax),
+      temperaturaMinima: parseFloat(tempMin),
     });
-  });
- 
-  let mediaTempAlte = tempAlte.reduce((a, b) => a + b, 0) / tempAlte.length;
-  let mediaTempBasse = tempBasse.reduce((a, b) => a + b, 0) / tempBasse.length;
-
-  
-
+  } else {
+    datiMeteo.push({
+      nome: stazione,
+      dati: [
+        {
+          temperaturaMassima: parseFloat(tempMax),
+          temperaturaMinima: parseFloat(tempMin),
+        },
+      ],
+    });
+  }
   save();
 });
 
-
-
 app.listen(port, () => {
-    console.log(`Server avviato su http://localhost:${port}`);
-  });
-  
-  
+  console.log(`Server avviato su http://localhost:${port}`);
+});
